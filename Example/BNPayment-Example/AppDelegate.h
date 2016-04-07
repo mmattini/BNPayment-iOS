0f3ce5c71067e5050c55e2d1b76d3f26365b861b
Oskar Henriksson <oskar.henriksson@slagkryssaren.com>
2016-04-07 15:45:41 +0200

    Initial commit

---
+++ Example/BNPayment-Example/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BNPayment-Example
+//
+//  Created by Oskar Henriksson on 07/04/2016.
+//  Copyright © 2016 Bambora. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Example/BNPayment-Example/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BNPayment-Example
+//
+//  Created by Oskar Henriksson on 07/04/2016.
+//  Copyright © 2016 Bambora. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
