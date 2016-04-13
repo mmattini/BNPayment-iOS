4da77b4c551bd72837c702a10b2789c4f7ddaea7
Oskar Henriksson <oskar.henriksson@slagkryssaren.com>
2016-04-13 16:43:26 +0200

    Added a little documentation to BNPaymentWebview protocol

--- BNPayment/UI/Webviews/BNPaymentWebview.h
+++ BNPayment/UI/Webviews/BNPaymentWebview.h
@@ -17,16 +17,44 @@
  *  `BNPaymentWebviewOperation`
  */
 typedef enum BNPaymentWebviewOperation : NSUInteger {
-    BNPWVOperationFetchURL,
-    BNPWVOperationLoadURL,
-    BNPWVOperationSubmitCCData
+    BNPWVOperationFetchURL,     // Fetches a URL to load in the webview.
+    BNPWVOperationLoadURL,      // Load the URL recieved in the webview.
+    BNPWVOperationSubmitCCData  // The user submits the credit card form.
 } BNPaymentWebviewOperation;
 
 @protocol BNPaymentWebviewDelegate <NSObject>
 
+/**
+ *  An authorized card is registered.
+ *
+ *  @param webview        The webview that contains the form.
+ *  @param authorizedCard The actual authorized card registered.
+ */
 - (void)BNPaymentWebview:(BNPaymentWebview *)webview didRegisterAuthorizedCard:(BNAuthorizedCreditCard *)authorizedCard;
+
+/**
+ *  An operation did start.
+ *
+ *  @param webview        The webview that contains the form.
+ *  @param operation      The operation started.
+ */
 - (void)BNPaymentWebview:(BNPaymentWebview *)webview didStartOperation:(BNPaymentWebviewOperation)operation;
+
+/**
+ *  An operation did finish successfully.
+ *
+ *  @param webview        The webview that contains the form.
+ *  @param operation      The operation that finished.
+ */
 - (void)BNPaymentWebview:(BNPaymentWebview *)webview didFinishOperation:(BNPaymentWebviewOperation)operation;
+
+/**
+ *  An operation did fail
+ *
+ *  @param webview        The webview that contains the form.
+ *  @param operation      The operation that failed.
+ *  @param error          The error that occured.
+ */
 - (void)BNPaymentWebview:(BNPaymentWebview *)webview didFailOperation:(BNPaymentWebviewOperation)operation withError:(NSError *)error;
 
 @end
@@ -35,6 +63,11 @@ typedef enum BNPaymentWebviewOperation : NSUInteger {
 
 @property (nonatomic, weak) id <BNPaymentWebviewDelegate> delegate;
 
+/**
+ *  Load the webview form with the `BNCCHostedFormParams` specified as input parameter.
+ *
+ *  @param params Params used to configure the form.
+ */
 - (void)loadPaymentWebviewWithParams:(BNCCHostedFormParams *)params;
 
 @end
