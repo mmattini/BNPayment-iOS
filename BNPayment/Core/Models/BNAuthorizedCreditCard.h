cddab343d84f0eab358a6c25752badfa9b7ccd46
Oskar Henriksson <oskar.henriksson@slagkryssaren.com>
2016-04-14 12:54:59 +0200

    Added more code documentation

--- BNPayment/Core/Models/BNAuthorizedCreditCard.h
+++ BNPayment/Core/Models/BNAuthorizedCreditCard.h
@@ -21,6 +21,16 @@
 @property (nonatomic, strong) NSNumber *creditCardExpiryMonth;
 @property (nonatomic, strong) NSNumber *creditCardExpiryYear;
 
+/**
+ *  A convenience method for creating an instance of `BNPaymentParams`
+ *
+ *  @param identifier `NSString` Representing the identifier of the payment.
+ *  @param currency   `NSString` Representing the ISO 4217 currency code of the payment.
+ *  @param amount     `NSString` Representing the amount in cents of the payment.
+ *  @param comment    `NSString` Reprenseting an optional comment for the payment.
+ *
+ *  @return An instance of `BNPaymentParams`
+ */
 - (BNPaymentParams *)paymentParamsWithId:(NSString *)identifier
                                 currency:(NSString *)currency
                                   amount:(NSNumber *)amount
--- BNPayment/Core/Models/BNCCHostedFormParams.h
+++ BNPayment/Core/Models/BNCCHostedFormParams.h
@@ -20,10 +20,43 @@
 @property (nonatomic, strong) NSArray<BNCCFormInputGroup *> *inputGroups;
 @property (nonatomic, strong) NSString *submitButtonText;
 
+/**
+ *  This method is used for setting the placeholder text for the credit card number input field in the hosted page.
+ *  This will be translated into setting the HTML placeholder attribute.
+ *
+ *  @param placeholder `NSString` representing the placeholder text.
+ */
 - (void)CCNumberPlaceholder:(NSString *)placeholder;
+
+/**
+ *  This method is used for setting the placeholder text for the expiry date input field in the hosted page.
+ *  This will be translated into setting the HTML placeholder attribute.
+ *
+ *  @param placeholder `NSString` representing the placeholder text.
+ */
 - (void)expiryPlaceholder:(NSString *)placeholder;
+
+/**
+ *  This method is used for setting the placeholder text for the CVV/CVC input field in the hosted page.
+ *  This will be translated into setting the HTML placeholder attribute.
+ *
+ *  @param placeholder `NSString` representing the placeholder text.
+ */
 - (void)cvvPlaceholder:(NSString *)placeholder;
 
+/**
+ *  This is a convenience method for creating `BNCCHostedFormParams` params.
+ *  This allows you to specify the CSS url used by the hosted page, all placeholders and the submit button text.
+ *  This can of course be done via the properties aswell.
+ *
+ *  @param css                   `NSString` containing and url to a CSS file.
+ *  @param cardNumberPlaceholder `NSString` representing the placeholder test for CC input group.
+ *  @param expiryPlaceholder     `NSString` representing the placeholder test for Expiry input group.
+ *  @param cvvPlaceholder        `NSString` representing the placeholder test for CVV/CVC input group.
+ *  @param submitText            `NSString` representing the text for the sumbit button.
+ *
+ *  @return An instance of `BNCCHostedFormParams`.
+ */
 + (BNCCHostedFormParams *)hostedFormParamsWithCSS:(NSString *)css
                             cardNumberPlaceholder:(NSString *)cardNumberPlaceholder
                                 expiryPlaceholder:(NSString *)expiryPlaceholder
