7f18f94936f137c7474db1dc0bfbdb2190b81ed9
Oskar Henriksson <oskar.henriksson@slagkryssaren.com>
2016-04-19 13:57:40 +0200

    Added BNCertManager

---
+++ BNPayment/Core/Security/BNCertManager.h
@@ -0,0 +1,13 @@
+//
+//  BNCertManager.h
+//  BNPayment
+//
+//  Created by Oskar Henriksson on 19/04/2016.
+//  Copyright © 2016 Bambora. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface BNCertManager : NSObject
+
+@end
---
+++ BNPayment/Core/Security/BNCertManager.m
@@ -0,0 +1,13 @@
+//
+//  BNCertManager.m
+//  BNPayment
+//
+//  Created by Oskar Henriksson on 19/04/2016.
+//  Copyright © 2016 Bambora. All rights reserved.
+//
+
+#import "BNCertManager.h"
+
+@implementation BNCertManager
+
+@end
